9ba76f9983fe5a70a39524e96e90ae52fe0c2bea
asnganje <abd.nganje@gmail.com>
2026-09-06 15:05:28 +0300

    add logic for the relevant ebay item

--- ebay_scraper/ebay_scraper/items.py
+++ ebay_scraper/ebay_scraper/items.py
@@ -5,9 +5,17 @@
 
 from dataclasses import dataclass
 
+import scrapy
+
 
 @dataclass
 class EbayScraperItem:
     # define the fields for your item here like:
     # name: str | None = None
     pass
+class EbayItem(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    condition = scrapy.Field()
+    quantity = scrapy.Field()
+    product_url = scrapy.Field()
